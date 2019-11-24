f7b948db0e5d58c181d13df0f91f9b06b35ed17a
snakeice45 <jonas171192@gmail.com>
2019-11-24 15:10:40 -0600

    frases matonas DB

---
+++ alumnosTable.sql
@@ -0,0 +1,23 @@
+/*drop schema generation;*/
+
+create database generation;
+use generation;
+create table Alumnos
+(
+	idAlumno int(45) primary key not null,
+    Nombre varchar(45) not null,
+    domicilio varchar(45),
+    edad int,
+    sexo varchar(15)
+);
+Select * from alumnos;
+
+insert into alumnos values (1,'Jonas','Zapopan',26,'Hombre'), 
+(2,'Jorge','Guadalara',29,'Hombre'),
+(3,'Daniel','Zapopan',24,'Hombre'),
+(4,'Ramiro','Zapopan',30,'Hombre');
+
+alter table alumnos add column idCohort int;
+alter table alumnos add foreign key(idCohort) references cohort(idCohort);
+
+
